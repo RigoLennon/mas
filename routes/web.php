056edba66027de7f2ec5a56f520101a3ce4852c9
Rigoberto Alcantar <a20150338@utem.edu.mx>
2020-11-11 16:45:38 -0600

    minimal changes

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,6 @@ Route::get('/admin/categories/delete/{id}', 'CategoriesController@destroy')->nam
 Route::get('/admin/products', 'ProductController@indexmas')->name('products');
 Route::get('/admin/products/create', 'ProductController@create')->name('newproduct');
 Route::post('/admin/products/post', 'ProductController@store')->name('postproduct');
+Route::get('/admin/products/edit/{id}', 'ProductController@edit')->name('editproduct');
+Route::put('/admin/products/update/{id}', 'ProductController@update')->name('updateproduct');
 Route::get('/admin/products/delete/{id}', 'ProductController@destroy')->name('deleteproduct');
