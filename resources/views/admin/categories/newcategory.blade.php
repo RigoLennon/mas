5b9954fffd0e61ed45518fdf035c6076762b5a34
Rigoberto Alcantar <a20150338@utem.edu.mx>
2020-11-26 16:50:12 -0600

    Minimal changes

--- resources/views/admin/categories/newcategory.blade.php
+++ resources/views/admin/categories/newcategory.blade.php
@@ -1,21 +1,21 @@
 @extends('admin.sidebar')
 
 @section('newcat')
-    <h1>Nueva categoria</h1>
-
     <div class="container">
+        <h1 class="cat-txt-center">Nueva categoria</h1>
         <div class="row">
             <div class="col-md-6">
                 <form method="POST" action="{{ route('postcategory') }}">
                     @csrf
-                    <div class="form-row">
-                        <div class="form-group">
-                            <label>Nombre de la categoria</label>
-                            <input type="text" name="cat_name" class="form-control" placeholder="Ingresa el nombre de la categoria" required>
+                    <div class="cardgrad-form-control">
+                        <h3>Nombre de la categoria</h3>
+                        <small class="font-italic" style="color: rgb(83, 83, 83)">*Esto te ayuda a organizar tus productos y que los clientes encuentren mas rapido lo que necesitan</small>
+                        
+                        <input type="text" name="cat_name" class="form-control" style="margin-top: 0.5em" placeholder="Ingresa el nombre de la categoria" required>
+
+                        <div style="margin-top: 0.5em">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
                         </div>
-                    </div>
-                    <div>
-                        <button type="submit" class="btn btn-primary">Guardar</button>
                     </div>
                 </form>
             </div>
