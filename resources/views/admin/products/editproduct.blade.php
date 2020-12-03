e7d028051c3842612a6a7d8260ed554619520966
Rigoberto Alcantar <a20150338@utem.edu.mx>
2020-12-02 19:03:41 -0600

    Mod estilos de los forms products

--- resources/views/admin/products/editproduct.blade.php
+++ resources/views/admin/products/editproduct.blade.php
@@ -1,48 +1,42 @@
 @extends('admin.sidebar')
 
 @section('editprod')
-    <h1>Editar producto</h1>
+<div class="container">
+    <div class="col-md-12">
+        <h1>Editar producto</h1>
+        <form method="POST" action="{{ route('updateproduct', $product->id) }}">
+        @method('PUT')
+        @csrf
 
-    <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                <form method="POST" action="{{ route('updateproduct', $product->id) }}">
-                    @method('PUT')
-                    @csrf
-                    <div class="form-row">
-                        <div class="form-group">
-                            <label>Nombre del producto</label>
-                            <input type="text" name="name" class="form-control" value="{{ $product->name }}" required>
+            <div class="col">
+                <label>Nombre del producto</label>
+                <input type="text" name="name" class="form-control" value="{{ $product->name }}" required>
+                
+                <label>Descripcion del producto</label>
+                <input type="text" name="description" class="form-control" value="{{ $product->description }}" required>
+                
+                <label>Descripcion corta</label>
+                <input type="text" name="short_descrip" class="form-control" value="{{ $product->short_descrip }}" required>
+            </div>
 
-                            <label>Descripcion del producto</label>
-                            <input type="text" name="description" class="form-control" value="{{ $product->description }}" required>
+            <div class="col">
+                <label>Precio</label>
+                <input type="number" name="price" class="form-control col-6" value="{{ $product->price }}" required>
 
-                            <label>Precio</label>
-                            <input type="number" name="price" class="form-control" value="{{ $product->price }}" required>
-
-                            <label>Descripcion corta</label>
-                            <input type="text" name="short_descrip" class="form-control" value="{{ $product->short_descrip }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Example select</label>
-                            <select class="form-control" name="cat_id" id="exampleFormControlSelect1">
-                                <option>Seleccionar categoria...<option>
-                                @foreach ($cat_list as $list)
-                                    @if ($list->cat_status == '1')
-                                        <option value="{{ $list->id }}">{{ $list->cat_name }}</option>
-                                    @else
-                                        No existen categorias 
-                                    @endif
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div>
-                        <button type="submit" class="btn btn-primary">Guardar</button>
-                    </div>
-                </form>
+                <label for="exampleFormControlSelect1">Example select</label>
+                <select class="form-control" name="cat_id" id="exampleFormControlSelect1" required>
+                    <option value="">Selecciona una categoria...</option>
+                        @foreach ($cat_list as $list)
+                            @if ($list->cat_status == '1')
+                                <option value="{{ $list->id }}">{{ $list->cat_name }}</option>
+                            @else
+                                No existen categorias 
+                            @endif
+                        @endforeach
+                </select>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/admin/products/newproduct.blade.php
+++ resources/views/admin/products/newproduct.blade.php
@@ -6,52 +6,40 @@
         <h1>Nuevo Producto</h1>
 
         <form method="POST" action="{{ route('postproduct') }}" enctype="multipart/form-data">
-            @csrf
-            <div class="form-row">
+        @csrf
+        <div class="row">
+            <div class="col">
+                <label for="nombreprod">Nombre del producto</label>
+                <input type="text" class="form-control" id="nombreprod" name="name" required>
+                <small id="nombreprodHelp" class="form-text text-muted">*Nombre del producto sin variaciones</small>
                 
-                    <div class="form-group">
-                        <label for="nombreprod">Nombre del producto</label>
-                        <input type="text" class="form-control" id="nombreprod" name="name" required>
-                        <small id="nombreprodHelp" class="form-text text-muted">*Nombre del producto sin variaciones</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="fulldescprod">Descripcion del producto</label>
-                        <textarea class="form-control" id="fulldescprod" rows="2" name="description" required></textarea>
-                        <small id="fulldescprodHelp" class="form-text text-muted">*Detalla lo mas que puedas tu producto</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="shrtdescprod">Descripcion corta del producto</label>
-                        <input type="text" class="form-control" id="shrtdescprod" name="short_descrip" required>
-                        <small id="shrtdescprodHelp" class="form-text text-muted">*Describe tu producto en menos de 50 caracteres</small>
-                    </div>
+                <label for="fulldescprod">Descripcion del producto</label>
+                <textarea id="fulldescprod" class="form-control" rows="2" name="description" required></textarea>
+                <small id="fulldescprodHelp" class="form-text text-muted">*Detalla lo mas que puedas tu producto</small>
                 
+                <label for="shrtdescprod">Descripcion corta del producto</label>
+                <input type="text" class="form-control" id="shrtdescprod" name="short_descrip" required>
+                <small id="shrtdescprodHelp" class="form-text text-muted">*Describe tu producto en menos de 50 caracteres</small>
             </div>
+            <div class="col">
+                <label for="price">Precio del producto</label>
+                <input type="number" class="form-control col-3" id="price" name="price" required>
+                <small id="priceHelp" class="form-text text-muted">*Precio total de tu producto</small>
             
-            <div class="form-row">
-                
-                    <div class="form-group">
-                        <label for="price">Precio del producto</label>
-                        <input type="number" class="form-control" id="price" name="price" required>
-                        <small id="priceHelp" class="form-text text-muted">*Precio total de tu producto</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="category">Elige una categoria</label>
-                        <select class="form-control" name="cat_id" required>
-                            <option value="">Selecciona una categoria...</option>
-                            @foreach ($cat_list as $list)
-                                <option value="{{ $list->id }}">{{ $list->cat_name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="img">Sube una foto de tu producto</label>
-                        <input type="file" class="form-control" id="img" name="image_1" required>
-                        <small id="img" class="form-text text-muted">*Imgen llamativa de tu producto</small>
-                    </div>
-                
+                <label for="category">Elige una categoria</label>
+                <select class="form-control col-6" name="cat_id" required>
+                    <option value="">Selecciona una categoria...</option>
+                    @foreach ($cat_list as $list)
+                        <option value="{{ $list->id }}">{{ $list->cat_name }}</option>
+                    @endforeach
+                </select>
+            
+                <label for="img">Sube una foto de tu producto</label>
+                <input type="file" class="form-control col-6" id="img" name="image_1" required>
+                <small id="img" class="form-text text-muted">*Imgen llamativa de tu producto</small>
             </div>
-
-            <button type="submit" class="btn btn-primary">Guardar</button>
+        </div>
+        <button type="submit" class="btn btn-primary">Guardar</button>
         </form>
     </div>
 </div>
